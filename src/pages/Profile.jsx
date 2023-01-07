413a6002723ec4ea89380b743768137d3d53a278
Vinicius José <viniciusjosedev@gmail.com>
2023-01-07 15:11:22 -0300

    refactor: requeriment 13 done

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,37 @@
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
 
 export default class Profile extends Component {
+  state = {
+    loading: true,
+    userInfo: undefined,
+  };
+
+  async componentDidMount() {
+    const userInfo = await getUser();
+    this.setState({
+      userInfo,
+      loading: false,
+    });
+  }
+
   render() {
+    const { loading, userInfo } = this.state;
     return (
-      <h1 data-testid="page-profile">Profile</h1>
+      <div data-testid="page-profile">
+        <h1>Profile</h1>
+        {!loading ? <img
+          data-testid="profile-image"
+          src={ userInfo.image }
+          alt={ userInfo.name }
+        />
+          : <h1>Carregando...</h1>}
+        {!loading ? <h1>{userInfo.name}</h1> : ''}
+        {!loading ? <h1>{userInfo.email}</h1> : ''}
+        {!loading ? <h1>{userInfo.description}</h1> : ''}
+        {!loading ? <Link to="/profile/edit">Editar perfil</Link> : ''}
+      </div>
     );
   }
 }
