2107dcd2728fd526c57b010a840e94821d25108c
Vinicius José <viniciusjosedev@gmail.com>
2023-01-22 22:27:26 -0300

    more updates

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 import MusicCard from '../components/MusicCard';
+import style from '../style/Favorites.module.css';
+import { Spinner } from 'reactstrap';
 
 export default class Favorites extends Component {
   state = {
@@ -26,15 +28,19 @@ export default class Favorites extends Component {
   render() {
     const { loading, trackInfo } = this.state;
     return (
-      <main data-testid="page-favorites">
-        <h1>Favorites</h1>
-        {loading ? <h1>Carregando...</h1>
-          : trackInfo.map((elemento) => (
-            <MusicCard
-              key={ elemento.trackId }
-              forceAtt={ this.forceAtt }
-              trackInfo={ elemento }
-            />))}
+      <main data-testid="page-favorites" id={ style.main }>
+				<div id={ style.divMain }>
+					<h1 id={ style.header }>Favoritos</h1>
+						<div id={ style.divTrackInfo }>
+						{loading ? <Spinner color="danger" />
+							: trackInfo.map((elemento) => (
+								<MusicCard
+									key={ elemento.trackId }
+									forceAtt={ this.forceAtt }
+									trackInfo={ elemento }
+								/>))}
+					</div>
+				</div>
       </main>
     );
   }
